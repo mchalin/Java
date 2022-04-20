a6b830865bc623e9d1b7fbe71c7fcb4f2a8a070c
Maximiliano Chalin <mchalin@gmail.com>
2022-04-19 21:32:32 -0300

    Mi primer commit de Java

---
+++ Leccion1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,18 @@
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        String saludo = "\nHola Mundo!\n";
+        System.out.println(saludo);
+        //Variable tipo int
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        //Variable tipo String
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Otro texto";
+        System.out.println(miVariableCadena);
+    }
+}
