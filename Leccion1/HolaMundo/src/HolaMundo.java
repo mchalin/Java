04bd1c829811bc7c8042647329db043d957f24dd
Maximiliano Chalin <mchalin@gmail.com>
2022-04-23 11:58:54 -0300

    Se agregaron cositas para que quede mas prolijo

--- Leccion1/HolaMundo/src/HolaMundo.java
+++ Leccion1/HolaMundo/src/HolaMundo.java
@@ -2,16 +2,23 @@
 public class HolaMundo {
 
     public static void main(String[] args) {
-        String saludo = "\nHola Mundo!\n";
+        //Un clasico
+        String saludo = "Hola Mundo!\n";
         System.out.println(saludo);
+
         //Variable tipo int
         int miVariable = 10;
+        System.out.println("\nVariable tipo int..");
         System.out.println(miVariable);
+        System.out.println("Le cambiamos el valor a la variable...");
         miVariable = 5;
         System.out.println(miVariable);
+
         //Variable tipo String
         String miVariableCadena = "Bienvenidos";
+        System.out.println("\nVariable tipo String..");
         System.out.println(miVariableCadena);
+        System.out.println("Le cambiamos el valor a la variable...");
         miVariableCadena = "Otro texto";
         System.out.println(miVariableCadena);
     }
